6179e86776232eb7c7d6b1e1ee44ec5c0befc489
Josep Sayol <josep.sayol@gmail.com>
2019-04-29 17:06:27 +0200

    WS: add TODO for hosting

--- src/serve/hosting.js
+++ src/serve/hosting.js
@@ -17,6 +17,8 @@ var _attempts = 0;
 var server;
 
 function _startServer(options, config, port, init) {
+  // TODO(jsayol): figure out how to capture logging from `superstatic`
+  // (it will probably involve making changes to the package)
   server = superstatic({
     debug: true,
     port: port,
